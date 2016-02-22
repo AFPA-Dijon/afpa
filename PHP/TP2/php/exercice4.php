b7b0b3f2a9fd92696f4054ccc8385d427b444ebe
reithh <sy.jebrane13@gmail.com>
2016-02-22 10:40:04 +0000

    Correction exercice 4

--- PHP/TP2/php/exercice4.php
+++ PHP/TP2/php/exercice4.php
@@ -1,25 +1,49 @@
-<?php
-$max = 10;
+<?php 
+$maxTable = 10;
+$maxLignes = 10;
 ?>
 <div class="row">
+    <h4>Table de multiplication à la demande</h4>
     <div class="col s6">
-        <form method = "post" action="index.php?page=exercice4">
+        <form method="post" action="index.php?page=exercice4">
+          
+            <div class="row">
+                <div class="input-field col s12">
+                    <select name="table">
+                        <?php for($i = 1; $i<= ( isset($max)? $max: 10 ); $i++): ?>
+                        <option value="<?=$i?>"><?=$i?></option>
+                        <?php endfor; ?>
+                    </select>
+                    <label>Choisissez votre table</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s12">
+                    <select name="lignes">
+                        <?php for($i = 1; $i<= ( isset($max)? $max: 10 ); $i++): ?>
+                        <option value="<?=$i?>"><?=$i?></option>
+                        <?php endfor; ?>
+                    </select>
+                    <label>Choisissez le nombre de lignes</label>
+                </div>
+            </div>
+            <div class="row">
+                <button class="btn waves-effect waves-light" type="submit" name="action">Calculer
+                </button>
+            </div>
         </form>
-        <div class="row">
-            <h1>Table de multiplication à la demande</h1>
-        </div>
-        <div class="row">
-            <div class="input-field col s12">
-                <select name="table">
-                  
-                </select>
-                <label>Materialize Select</label>
-           </div>
-        </div>
-        <div class="row"></div>
-        <div class="row"></div>
     </div>
+    <?php if(!empty($_POST['lignes']) && !empty($_POST['table'])): ?>
     <div class="col s6">
-        
+        <h6>Résultats</h6>
+        <?php 
+        for($i = 1; $i <= $_POST['lignes']; $i++){
+            $resultat = $_POST['table'] * $i; 
+            echo $i . " x ". $_POST['table'] . " = " .  $resultat . "<br />";
+        } 
+        ?>
     </div>
+    <?php endif; ?>
+  
+    
 </div>
